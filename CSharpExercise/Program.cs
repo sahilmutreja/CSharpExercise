a9b6a30735a0c0fc1d33d35dd6758be22f563c0c
Sahil Mutreja <sahil.mutreja123@gmail.com>
2017-11-26 23:26:03 +0530

    Initial Commit

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -10,6 +10,8 @@ namespace CSharpExercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
